c4bdd9c369a83bbd2a7dc545b72c88139882ca67
Antonio Henrique <ahlr_2000@yahoo.com>
2017-08-10 09:48:21 -0300

    Correção do comando data no script mvrejsgr.sh

--- slackonfig.sh
+++ slackonfig.sh
@@ -61,8 +61,8 @@ sleep 3
 	echo "#Mover os arquivos de rejeitados e francesinha do BNB" >> /etc/cron.daily/mvrejsgr.sh
 	echo "pasta_origem=/home/ahlr/Downloads" >> /etc/cron.daily/mvrejsgr.sh
 	echo "pasta_destino=/home/ahlr/Dropbox/NET4YOU/NET4YOU/Bancos/BNB/Arquivos" >> /etc/cron.daily/mvrejsgr.sh
-	echo "mv \$pasta_origem/rel*.pdf  \$pasta_origem/Rejeitados-\`"date +%d_%m"\`.pdf" >> /etc/cron.daily/mvrejsgr.sh
-	echo "mv \$pasta_origem/srg*.pdf  \$pasta_origem/Francesinha-\`"date +%d_%m"\`.pdf" >> /etc/cron.daily/mvrejsgr.sh
+	echo "mv \$pasta_origem/rel*.pdf  \$pasta_origem/Rejeitados-\`"date -d \"-1 day\" +\"%d_%m\""\`.pdf" >> /etc/cron.daily/mvrejsgr.sh
+	echo "mv \$pasta_origem/srg*.pdf  \$pasta_origem/Francesinha-\`"date -d \"-1 day\" +\"%d_%m\""\`.pdf" >> /etc/cron.daily/mvrejsgr.sh
 	echo "mv \$pasta_origem/Francesinha*.pdf \$pasta_destino" >> /etc/cron.daily/mvrejsgr.sh
 	echo "mv \$pasta_origem/Rejeitados*.pdf \$pasta_destino" >> /etc/cron.daily/mvrejsgr.sh
 	chmod +x /etc/cron.daily/mvrejsgr.sh
